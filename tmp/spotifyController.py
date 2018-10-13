4e9700f7366d84bc17b46725a128449265c77191
Adam Keefer <keefera@umich.edu>
2018-10-13 11:01:46 -0400

    adding better functionality

--- tmp/spotifyController.py
+++ tmp/spotifyController.py
@@ -6,33 +6,37 @@ from spotipy.oauth2 import SpotifyClientCredentials
 # import pyrebase
 
 # consts
-'''
+
 CLIENT_ID = 'ffbf57e77cdc4d1f98b80bce63e16341'
 CLIENT_SECRET = '1e612801fe654e4b956b07448461ee67'
 REDIRECT_URL = 'http://localhost:8000'
-'''
+
 
 # Function plays song on spotify - limited to first result of query
 # pass in spotify username, artist name, and track name
 def playSong(username, artistName, trackName):
     scope = "streaming app-remote-control user-modify-playback-state user-read-playback-state"
-    token = util.prompt_for_user_token(username, scope)
+    token = util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
 
     sp = spotipy.Spotify(auth=token)
     results = sp.search(q='artist:' + artistName + ' track:' + trackName, type='track', limit=1)
 
-    if not results['tracks']['items']:
-        print("Error: No results found")
-        exit()
+    try:
+      trackURI = results['tracks']['items'][0]['uri']
+    except:
+      return "Error: No results found!"
 
-    trackURI = results['tracks']['items'][0]['uri']
+    print(sp.current_user())
+
     devices = sp.devices()
-    if not devices['devices']:
-        print("Error: Spotify must be open on one of your devices!")
-        exit()
 
-        deviceID = devices['devices'][0]['id']
-        sp.start_playback(device_id=deviceID, uris=[trackURI])
+    try:
+      deviceID = devices['devices'][0]['id']
+      sp.start_playback(device_id=deviceID, uris=[trackURI])
+    except:
+      return "Error: Spotify must be open on one of your devices!"
+
+    return "Now playing: " + trackName + " by " + artistName
 
 
 # Initialize Firebase with service account credentials
@@ -71,4 +75,4 @@ if (__name__ == '__main__'):
 
     # db = initdb()
 
-    playSong(sys.argv[1], sys.argv[2], sys.argv[3])
+    print(playSong(sys.argv[1], sys.argv[2], sys.argv[3]))
