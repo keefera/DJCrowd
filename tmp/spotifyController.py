c0a30e2f64aee28cf6bbd857bfbf1f567249d1ff
Adam Keefer <keefera@umich.edu>
2018-10-13 10:29:03 -0400

    blah

--- tmp/spotifyController.py
+++ tmp/spotifyController.py
@@ -6,18 +6,17 @@ from spotipy.oauth2 import SpotifyClientCredentials
 # import pyrebase
 
 # consts
+'''
 CLIENT_ID = 'ffbf57e77cdc4d1f98b80bce63e16341'
 CLIENT_SECRET = '1e612801fe654e4b956b07448461ee67'
 REDIRECT_URL = 'http://localhost:8000'
-
+'''
 
 # Function plays song on spotify - limited to first result of query
 # pass in spotify username, artist name, and track name
 def playSong(username, artistName, trackName):
     scope = "streaming app-remote-control user-modify-playback-state user-read-playback-state"
-    token = util.prompt_for_user_token(username, scope, client_id=CLIENT_ID,
-                                       client_secret=CLIENT_SECRET,
-                                       redirect_uri=REDIRECT_URL)
+    token = util.prompt_for_user_token(username, scope)
 
     sp = spotipy.Spotify(auth=token)
     results = sp.search(q='artist:' + artistName + ' track:' + trackName, type='track', limit=1)
